c9e0fbe654ce05031e1457080baf37153635fc25
ReaperMaga <reapermaga@gmail.com>
2021-11-30 04:11:31 +0100

    Added auth controller

--- src/test/java/net/woolgens/user/UserResourceTest.java
+++ src/test/java/net/woolgens/user/UserResourceTest.java
@@ -3,7 +3,6 @@ package net.woolgens.user;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import net.woolgens.user.auth.AuthWrapper;
 import net.woolgens.user.model.User;
 import net.woolgens.user.repository.UserRepository;
 import net.woolgens.user.resource.UserResource;
@@ -11,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
-
 import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
@@ -21,10 +19,7 @@ import static org.hamcrest.CoreMatchers.is;
 @TestHTTPEndpoint(UserResource.class)
 public class UserResourceTest {
 
-    @Inject
-    AuthWrapper authWrapper;
 
-    String token;
 
     @Inject
     UserRepository repository;
@@ -34,16 +29,12 @@ public class UserResourceTest {
     public void setup() {
         user = new User(UUID.randomUUID().toString());
         repository.persist(user);
-        if(token == null) {
-            token = authWrapper.getBootstrap().getProvider().generateToken("Test", "Admin", 5);
-        }
     }
 
 
     @Test
     public void testGetEndpoint() {
         given()
-                .header("Authorization", "Bearer " + token)
                 .when().get("/" + user.getUuid())
                 .then()
                 .statusCode(200)
@@ -56,7 +47,6 @@ public class UserResourceTest {
         User newUser = new User(uuid);
 
         given().body(newUser).contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
                 .when().post()
                 .then()
                 .statusCode(200)
@@ -66,7 +56,6 @@ public class UserResourceTest {
     @Test
     public void testDeleteEndpoint() {
         given()
-                .header("Authorization", "Bearer " + token)
                 .when().delete("/" + user.getUuid())
                 .then()
                 .statusCode(200);
